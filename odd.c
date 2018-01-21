028a1eb3c9e345df1fe11b1db34bd8ff66fdaacd
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-21 10:16:11 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int number,m=23,n=43;
+printf("print the odd number in the given range"\n);
+for(number=m;number<=43;number++)
+{
+if(number%2=1)
+{
+printf("the number is odd");
+}
+else
+{
+printf("the number is even");
+}
+return 0;
+}
+}
