7b312f5dd3e52d5bd76d0cc50b72689c338dd331
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-21 10:26:31 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int number,m=1,n=10;
+printf("print even numbers in a given range":\n);
+for("number=m;number<=10;number);
+{
+if(number/2=0)
+{
+printf("the number is even");
+}
+else
+{
+printf("number is odd");
+}
+}
+}
